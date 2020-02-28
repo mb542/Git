935859175fc3630f6666ca050311a2bdda4ac1f9
gy <4997194@qq.com>
2020-02-28 20:08:18 +0800

    这是一个查询所有用户的功能

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -8,7 +8,4 @@ public interface UserDao {
     public List<user> showall();
     public void add(user user);
     public void delete();
-    public void update();
-    public void show();
-    public user queryone();
 }
